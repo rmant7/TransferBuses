7ff63376af782f4fd5a66be3e6e8b17d4642f229
Leonid <ajer333@gmail.com>
2021-07-02 14:53:13 +0300

    test commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-
+import {getUser} from './services/test-servises';
 import './App.css';
 
 function App() {
+  getUser('HWbiCRaDI8WhI8QWCWcL');
   return (
     <h1>transfer-buses start!</h1>
   );
