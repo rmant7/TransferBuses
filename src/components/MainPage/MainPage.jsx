7319113f06c326aa90be3030774152bc6c21d611
Yury Vinogradov <yuvinogradov@gmail.com>
2021-07-19 20:44:23 +0300

    added i18n to MainPage.jsx

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,11 +1,12 @@
 import "./MainPage.css";
 import { useHistory } from "react-router-dom";
+import i18n from "../../i18n";
 
 export default function MainPage(){
   const history = useHistory();
   return <div className="MainPage">
-    <button className="btn" onClick={() => history.push("/viewRoutes")}>Я турист</button>
+    <button className="btn" onClick={() => history.push("/viewRoutes")}>{i18n.t("I'm a passenger")}</button>
     {/* <button className="btn" onClick={() => history.push("/addRoute")}>Я извозчик</button> */}
-    <button className="btn" onClick={() => history.push("/addTransfer")}>Я водитель</button>
+    <button className="btn" onClick={() => history.push("/addTransfer")}>{i18n.t("I'm a driver")}</button>
   </div>
 }
