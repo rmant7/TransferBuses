838ef6e356484f36f0c9305814ce9613bf62e3ba
Sergey <4923920@gmail.com>
2021-08-04 00:00:09 +0300

    main file for all components

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Navbar } from './Navbar/Navbar';
+export { default as BusPage } from './BusPage/BusPage';
+export { default as DriverPage } from './DriverPage/DriverPage';
+export { default as MainPage } from './MainPage/MainPage';
+export { default as PassengerPage } from './PassengerPage/PassengerPage';
+export { default as TransferPage } from './TransferPage/TransferPage';
+export { default as AppRouter } from './AppRouter/AppRouter';
