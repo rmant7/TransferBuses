aa2c6891ab6c4b35d38cfcb37693d941c2c0216a
Sergey <4923920@gmail.com>
2021-08-03 23:57:04 +0300

    refactored code, added AppRouter, added ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,47 +1,32 @@
-import {Route, Switch} from "react-router-dom";
-import "./App.css";
-import MainPage from "./components/MainPage/MainPage";
-import BusPage from "./components/BusPage/BusPage";
-import PassengerPage from "./components/PassengerPage/PassengerPage";
-import DriverPage from "./components/DriverPage/DriverPage";
-import TransferPage from "./components/TransferPage/TransferPage";
-import Header from "./components/Header/Header";
-import {useTranslation} from "react-i18next"
-import {useEffect} from "react";
+import './App.css';
+import { AppRouter, Navbar } from './components';
+import { ThemeProvider } from '@material-ui/core/styles';
 
-
+import Header from './components/Header/Header';
+import { useTranslation } from 'react-i18next';
+import { useEffect } from 'react';
+import { theme } from './utils/useStyles';
 function App() {
-  const {i18n} = useTranslation();
+  const { i18n } = useTranslation();
   const changeLanguage = (language) => {
-    i18n.changeLanguage(language)
-      .then(
-      )
-      .catch(err => console.error(err))
-  }
+    i18n
+      .changeLanguage(language)
+      .then()
+      .catch((err) => console.error(err));
+  };
 
-  i18n.reloadResources()
-    .then(
-    )
+  i18n.reloadResources().then();
 
   useEffect(() => {
-    changeLanguage(localStorage.getItem('locale') || 'en')
+    changeLanguage(localStorage.getItem('locale') || 'en');
   }, []);
 
   return (
-    <div>
-      <Route path="/"
-             render={() => (
-               <Header changeLanguage={changeLanguage}/>
-             )}/>
-      <Switch>
-        <Route path="/" exact component={MainPage}/>
-        <Route path="/viewRoutes" component={PassengerPage}/>
-        <Route path="/addRoute" component={BusPage}/>
-        <Route path="/addTransfer" component={DriverPage}/>
-        <Route path="/transfer" component={TransferPage}/>
-      </Switch>
-
-    </div>
+    <ThemeProvider theme={theme}>
+      <Navbar />
+      {/* <Route path="/" render={() => <Header changeLanguage={changeLanguage} />} /> */}
+      <AppRouter />
+    </ThemeProvider>
   );
 }
 
