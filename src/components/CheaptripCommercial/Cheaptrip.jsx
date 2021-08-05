1054a4a1eb3ead596720209381b57f59f7c2e6b7
Yura Dukhno <yura.dukhno@gmail.com>
2021-08-05 05:38:28 +0300

    add multi language commercial text

--- src/components/CheaptripCommercial/Cheaptrip.jsx
+++ src/components/CheaptripCommercial/Cheaptrip.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Cheaptrip.css";
 import Logo_Cht from "./Logo_ChT_2.png";
+import i18n from "../../i18n";
 
 export default function Cheaptrip() {
   return (
@@ -8,12 +9,13 @@ export default function Cheaptrip() {
       <div className="partners">
         <h3>Our partners:</h3>
       </div>
-      <div className="title">CheapTrip Pay less,visit more!</div>
+
       <div className="caption">
         <a href="https://cheaptrip.guru/">
           <img src={Logo_Cht} alt="CheapTrip Logo" />
         </a>
       </div>
+      <div className="title">{i18n.t("CheapTrip commercial")}</div>
     </div>
   );
 }
