4134763b2b39583e06ce680de19defdc91c415c3
Leonid <ajer333@gmail.com>
2021-08-06 23:56:06 +0300

    test

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,21 +1,12 @@
 import './MainPage.css';
 import { useHistory } from 'react-router-dom';
-import { createTheme, ThemeProvider } from '@material-ui/core/styles';
 import i18n from '../../i18n';
 import Button from '@material-ui/core/Button';
 
-const theme = createTheme({
-  palette: {
-    secondary: {
-      main: '#607d8b',
-    },
-  },
-});
 export default function MainPage() {
   const history = useHistory();
   return (
     <div className="MainPage">
-      <ThemeProvider theme={theme}>
         <Button
           color="secondary"
           variant="contained"
@@ -30,7 +21,6 @@ export default function MainPage() {
           onClick={() => history.push('/addTransfer')}>
           {i18n.t("I'm a driver")}
         </Button>
-      </ThemeProvider>
     </div>
   );
 }
