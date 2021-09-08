7dd2fc0dfecc8b37dd738c5155517d202d0fd12b
Yury Vinogradov <41233522+yuvinogradov@users.noreply.github.com>
2021-09-08 18:28:27 +0300

    fix: transfer page - selected regular trips icons

--- src/components/WeekDayIcon/WeekDayIcon.jsx
+++ src/components/WeekDayIcon/WeekDayIcon.jsx
@@ -4,14 +4,16 @@ import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked";
 import i18n from "i18next";
 
 export const WeekDayIcon = ({ name, value }) => {
+  console.log("WeekDayIcon name, value: ", name, value, value.selected)
   return (
     <>
       <div>
         <div style={{ textAlign: "center" }}>
           {i18n.t(name).slice(0,1).toUpperCase()}
         </div>
-        <div>{value && <RadioButtonCheckedIcon />}</div>
-        <div>{!value && <RadioButtonUncheckedIcon />}</div>
+        {/*<div>{value.selected && <RadioButtonCheckedIcon />}</div>*/}
+        {/*<div>{!value.selected && <RadioButtonUncheckedIcon />}</div>*/}
+        <div title={value.departureTime}>{value.selected ? <RadioButtonCheckedIcon /> : <RadioButtonUncheckedIcon />}</div>
       </div>
     </>
   );
