6e0691006b9df9084a35096490aa088ea576f2e5
Edward {•...•} <59051661+Justedlev@users.noreply.github.com>
2021-10-23 16:04:49 +0300

    Fixed code

--- src/redux/actions/filters-actions.js
+++ src/redux/actions/filters-actions.js
@@ -3,27 +3,28 @@ import { loadingTransfersAction } from "./loading-actions";
 import { SET_TRANSFERS } from "./transfers-actions";
 
 export const SET_FILTERS = "set-filters";
+export const SET_FILTER_APPLY = "set-filters-apply";
 export const SET_SELECT_FILTER = "set-select-filter";
 
 export function applyFilterFromCityIdAction(fromCityId) {
   return async (dispatch) => {
+    dispatch(loadingTransfersAction(true));
     try {
-      dispatch(loadingTransfersAction(true));
-      dispatch({
-        type: SET_TRANSFERS,
-        payload: { isReceived: false, transfers: [] },
-      });
       const filteredCities = await getTransfersByFromCityId(fromCityId);
       dispatch({
-        type: SET_TRANSFERS,
-        payload: { isReceived: true, transfers: filteredCities.slice() },
+        type: SET_FILTER_APPLY,
+        payload: true,
       });
-      dispatch(loadingTransfersAction(false));
+      dispatch({
+        type: SET_TRANSFERS,
+        payload: filteredCities.slice(),
+      });
     } catch (e) {
       dispatch({
         type: SET_TRANSFERS,
-        payload: { isReceived: false, transfers: [] },
+        payload: [],
       });
+    } finally {
       dispatch(loadingTransfersAction(false));
     }
   };
