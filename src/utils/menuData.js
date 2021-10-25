f9b554af051c63773496fc0a6009f31bacde20a3
Edward {•...•} <59051661+Justedlev@users.noreply.github.com>
2021-10-25 21:29:15 +0300

    Fixed path constants names

--- src/utils/menuData.js
+++ src/utils/menuData.js
@@ -1,9 +1,8 @@
-import { MAIN_ROUTE, PASSENGER_ROUTE, DRIVER_ROUTE, CONTACTS_ROUTE } from './constants';
-
+import { ADD_TRANSFER_ROUTE, CONTACTS_ROUTE, MAIN_ROUTE, TRANSFERS_ROUTE } from "./constants";
 
 export const menuData = [
   { title: "Home", path: MAIN_ROUTE },
-  { title: "I\u0027m a passenger", path: PASSENGER_ROUTE },
-  { title: "I\u0027m a driver", path: DRIVER_ROUTE },
+  { title: "I\u0027m a passenger", path: TRANSFERS_ROUTE },
+  { title: "I\u0027m a driver", path: ADD_TRANSFER_ROUTE },
   { title: "Contacts", path: CONTACTS_ROUTE },
 ];
