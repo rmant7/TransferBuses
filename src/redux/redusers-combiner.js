9b1a67c5339219dae6c9e630ec1a9aa7198a8bc9
Edward {•...•} <59051661+Justedlev@users.noreply.github.com>
2021-10-25 21:11:31 +0300

    Added new reducer

--- src/redux/redusers-combiner.js
+++ src/redux/redusers-combiner.js
@@ -2,13 +2,14 @@ import { combineReducers } from "redux";
 import appReducer from "./reducers/app-reducer";
 import { filtersReducer } from "./reducers/filters-reducer";
 import { loadingReducer } from "./reducers/loading-reducer";
-import { saveNewTransferReducer } from "./reducers/transfer-upload-reducer";
+import { saveNewTransferReducer, transferReducer } from "./reducers/transfer-reducer";
 import { transfersReducer } from "./reducers/transfers-reducer";
 
 export const rootReducer = combineReducers({
-    app: appReducer,
-    transfers: transfersReducer,
-    saveNewTransfer: saveNewTransferReducer,
-    filters: filtersReducer,
-    loading: loadingReducer
+  app: appReducer,
+  transfers: transfersReducer,
+  transfer: transferReducer,
+  saveNewTransfer: saveNewTransferReducer,
+  filters: filtersReducer,
+  loading: loadingReducer,
 });
