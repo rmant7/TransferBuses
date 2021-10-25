729c2994b0b8a45e919c59959c0d25454ab316d1
Edward {•...•} <59051661+Justedlev@users.noreply.github.com>
2021-10-25 21:11:54 +0300

    Renamed and updated

---
+++ src/redux/actions/transfer-actions.js
@@ -0,0 +1,47 @@
+import { getTransfer, uploadNewTransfer } from "../../services/data-service";
+import { loadingTransferAction, loadingUploadTransferAction } from "./loading-actions";
+
+export const SET_TRANSFER_IS_RECEIVED = "set-transfer-is-received";
+export const SET_TRANSFER_IS_SAVED = "set-transfer-is-saved";
+export const SET_TRANSFER_DATA = "set-transfer-data";
+export const SET_TRANSFER_MESSAGE = "set-transfer-message";
+
+export function getTransferAction(id) {
+  return async (dispatch) => {
+    dispatch(loadingTransferAction(true));
+    try {
+      const transfer = await getTransfer(id);
+      dispatch({
+        type: SET_TRANSFER_IS_RECEIVED,
+        payload: true,
+      });
+      dispatch({
+        type: SET_TRANSFER_DATA,
+        payload: transfer,
+      });
+    } catch (e) {
+      dispatch({ type: SET_TRANSFER_IS_RECEIVED, payload: false });
+      dispatch({ type: SET_TRANSFER_MESSAGE, payload: e });
+    } finally {
+      dispatch(loadingTransferAction(false));
+    }
+  };
+}
+
+export function saveNewTransferAction(transfer) {
+  return async (dispatch) => {
+    dispatch(loadingUploadTransferAction(true));
+    try {
+      await uploadNewTransfer(transfer);
+      dispatch({
+        type: SET_TRANSFER_IS_SAVED,
+        payload: true,
+      });
+    } catch (e) {
+      dispatch({ type: SET_TRANSFER_IS_SAVED, payload: false });
+      dispatch({ type: SET_TRANSFER_MESSAGE, payload: e });
+    } finally {
+      dispatch(loadingUploadTransferAction(false));
+    }
+  };
+}
---
+++ src/redux/reducers/transfer-reducer.js
@@ -0,0 +1,41 @@
+import {
+  SET_TRANSFER_DATA,
+  SET_TRANSFER_IS_SAVED,
+  SET_TRANSFER_MESSAGE,
+  SET_TRANSFER_IS_RECEIVED,
+} from "../actions/transfer-actions";
+
+const initialTransferState = {
+  isReceived: false,
+  data: {},
+  msg: "",
+};
+
+const newTransferState = {
+  isSaved: false,
+  msg: "",
+};
+
+export function transferReducer(state = initialTransferState, action) {
+  switch (action.type) {
+    case SET_TRANSFER_IS_RECEIVED:
+      return { ...state, isReceived: action.payload };
+    case SET_TRANSFER_DATA:
+      return { ...state, data: action.payload };
+    case SET_TRANSFER_MESSAGE:
+      return { ...state, msg: action.payload };
+    default:
+      return state;
+  }
+}
+
+export function saveNewTransferReducer(state = newTransferState, action) {
+  switch (action.type) {
+    case SET_TRANSFER_IS_SAVED:
+      return { ...state, isSaved: action.payload };
+    case SET_TRANSFER_MESSAGE:
+      return { ...state, msg: action.payload };
+    default:
+      return state;
+  }
+}
