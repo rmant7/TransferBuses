afba308e6af4962577194eedf37616b7cf279a6a
Edward {•...•} <59051661+Justedlev@users.noreply.github.com>
2021-10-26 22:16:11 +0300

    Updated

--- src/utils/months-util.ts
+++ src/utils/months-util.ts
@@ -1,3 +1,135 @@
+type Month = {
+  number: number;
+  name: string;
+  short: string;
+};
+
+export const monthsRus: Month[] = [
+  {
+    number: 1,
+    name: "Январь",
+    short: "Янв",
+  },
+  {
+    number: 2,
+    name: "Февраль",
+    short: "Фев",
+  },
+  {
+    number: 3,
+    name: "Март",
+    short: "Мар",
+  },
+  {
+    number: 4,
+    name: "Апрель",
+    short: "Апр",
+  },
+  {
+    number: 5,
+    name: "Май",
+    short: "Май",
+  },
+  {
+    number: 6,
+    name: "Июнь",
+    short: "Июн",
+  },
+  {
+    number: 7,
+    name: "Июль",
+    short: "Июл",
+  },
+  {
+    number: 8,
+    name: "Август",
+    short: "Авг",
+  },
+  {
+    number: 9,
+    name: "Сентябрь",
+    short: "Сен",
+  },
+  {
+    number: 10,
+    name: "Октябрь",
+    short: "Окт",
+  },
+  {
+    number: 11,
+    name: "Ноябрь",
+    short: "Ноя",
+  },
+  {
+    number: 12,
+    name: "Декабрь",
+    short: "Дек",
+  },
+];
+
+export const monthsEng: Month[] = [
+  {
+    number: 1,
+    name: "January",
+    short: "Jan",
+  },
+  {
+    number: 2,
+    name: "February",
+    short: "Feb",
+  },
+  {
+    number: 3,
+    name: "March",
+    short: "Mar",
+  },
+  {
+    number: 4,
+    name: "April",
+    short: "Apr",
+  },
+  {
+    number: 5,
+    name: "May",
+    short: "May",
+  },
+  {
+    number: 6,
+    name: "June",
+    short: "Jun",
+  },
+  {
+    number: 7,
+    name: "July",
+    short: "Jul",
+  },
+  {
+    number: 8,
+    name: "August",
+    short: "Aug",
+  },
+  {
+    number: 9,
+    name: "September",
+    short: "Sep",
+  },
+  {
+    number: 10,
+    name: "October",
+    short: "Oct",
+  },
+  {
+    number: 11,
+    name: "November",
+    short: "Nov",
+  },
+  {
+    number: 12,
+    name: "December",
+    short: "Dec",
+  },
+];
+
 export const months_ru: string[] = [
   "Янв.",
   "Фев.",
