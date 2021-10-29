278b55261f77380c059d799105aa830715396f58
Edward {•...•} <59051661+Justedlev@users.noreply.github.com>
2021-10-29 12:48:18 +0300

    Rewrited the util to typescript

---
+++ src/utils/cities-util.ts
@@ -0,0 +1,26 @@
+import cities from "./cities.json";
+
+type City = {
+  ID: string;
+  name: string;
+  country_id: string;
+  latitude: string;
+  longitude: string;
+  name_ru: string;
+};
+
+export function getCities(): City[] {
+  return cities;
+}
+
+export function getCityById(id: string): City | undefined {
+  return cities.find((city) => city.ID === id);
+}
+
+export function getCityByName(name: string): City | undefined {
+  return cities.find((city) => city.name === name);
+}
+
+export function getCityByNameRu(name: string): City | undefined {
+  return cities.find((city) => city.name_ru === name);
+}
