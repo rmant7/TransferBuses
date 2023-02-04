9a94de9473924e8fca9dd3916410e6a2c6e81b8d
Bittersweet-girl <enatav1@gmail.com>
2023-02-04 20:33:15 +0200

    FEAT: add and fix buttons

--- src/components/SearchResult/SearchResultItem.jsx
+++ src/components/SearchResult/SearchResultItem.jsx
@@ -66,7 +66,12 @@ export default function SearchResultItem({ item }) {
           {defineTypeOfTransport(item)}
           <Link href='https://www.booking.com/index.en-gb.html' target="_blank" rel="noreferrer">
             <Button variant="outlined" style={resultItemStyle.buyTicket}>
-              Find stay
+              Booking
+            </Button>
+          </Link>
+          <Link href='https://www.hostelworldgroup.com' target="_blank" rel="noreferrer">
+            <Button variant="outlined" style={resultItemStyle.buyTicket}>
+              Hostel world
             </Button>
           </Link>
         </Box>
