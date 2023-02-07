c454a1491a58f8986cc9467e521688c44bfb7eed
Bittersweet-girl <enatav1@gmail.com>
2023-02-07 15:09:13 +0200

    FEAT: add mobile style

--- src/components/SearchResult/SearchResultItem.jsx
+++ src/components/SearchResult/SearchResultItem.jsx
@@ -5,29 +5,32 @@ import DirectionsBoatIcon from '@mui/icons-material/DirectionsBoat';
 import TrainIcon from '@mui/icons-material/Train';
 import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
 import { resultItemStyle } from './style';
-import CarSharing from "../../assets/car-sharing.png"
+import CarSharing from "../../assets/car-sharing.png";
+import useMediaQuery from '@mui/material/useMediaQuery';
+
 
 export default function SearchResultItem({ item }) {
+  const style = useMediaQuery('(max-width:650px)') ? resultItemStyle.sm : resultItemStyle.lg;
   const defineTypeOfTransport = (transport) => { // TODO change cases to names from database
     let resultLink;
     switch (transport) {
       case 2:
         resultLink = (<Link href='https://blablacar.com' target="_blank" rel="noreferrer">
-          <Button variant="outlined" style={resultItemStyle.buyTicket} type="submit">
+          <Button variant="outlined" style={style.buyTicket} type="submit">
             Find a car
           </Button>
         </Link>);
         break;
       case 3:
         resultLink = (<Link href='https://www.aferry.com/' target="_blank" rel="noreferrer">
-          <Button variant="outlined" style={resultItemStyle.buyTicket} type="submit">
+          <Button variant="outlined" style={style.buyTicket} type="submit">
             Buy Ticket
           </Button>
         </Link>);
         break;
       default:
         resultLink = (<Link href='https://omio.sjv.io/XxEWmb' target="_blank" rel="noreferrer">
-          <Button variant="outlined" style={resultItemStyle.buyTicket} type="submit">
+          <Button variant="outlined" style={style.buyTicket} type="submit">
             Buy Ticket
           </Button>
         </Link>);
@@ -38,44 +41,44 @@ export default function SearchResultItem({ item }) {
     let resultIcon;
     switch (transport) {
       case 2:
-        resultIcon = (<Typography><img src={CarSharing} alt='car-shearing' style={resultItemStyle.car} /> Car share</Typography>);
+        resultIcon = (<Typography style={style.iconText}>Car share <img src={CarSharing} alt='car-shearing' style={style.car} /></Typography>);
         break;
       case 3:
-        resultIcon = (<Typography><DirectionsBoatIcon style={resultItemStyle.icon} /> Ferry</Typography>);
+        resultIcon = (<Typography style={style.iconText}>Ferry <DirectionsBoatIcon style={style.icon} /></Typography>);
         break;
       case 4:
-        resultIcon = (<Typography><AirplanemodeActiveIcon style={resultItemStyle.icon} /> Flight</Typography>);
+        resultIcon = (<Typography style={style.iconText}>Flight <AirplanemodeActiveIcon style={style.icon} /></Typography>);
         break;
       case 5:
-        resultIcon = (<Typography><TrainIcon style={resultItemStyle.icon} /> Train</Typography>);
+        resultIcon = (<Typography style={style.iconText}>Train <TrainIcon style={style.icon} /></Typography>);
         break;
       default:
-        resultIcon = (<Typography><DirectionsBusIcon style={resultItemStyle.icon} /> Bus</Typography>);
+        resultIcon = (<Typography style={style.iconText}>Bus <DirectionsBusIcon style={style.icon} /></Typography>);
     }
     return resultIcon;
   }
   return (
-    <Box style={resultItemStyle.itemContainer}>
-      <Box style={resultItemStyle.directions}>
-        <Typography style={resultItemStyle.boldText}>Kiev - Wroclaw {item}</Typography>
+    <Box style={style.itemContainer}>
+      <Box style={style.directions}>
+        <Typography style={style.boldText}>Kiev - Wroclaw - {item}</Typography>
         {defineIconOfTransport(item)}
       </Box>
-      <Box style={resultItemStyle.directions}>
-        <Typography sx={{ color: 'rgb(119, 87, 80)' }}>3 h 55m</Typography>
-        <Box>
+      <Box style={style.directions}>
+        <Typography style={style.time}>3 h 55m</Typography>
+        <Box sx={useMediaQuery('(max-width:480px)') ? {display: 'flex', flexDirection: "column", alignItems: "center"} : {}}>
           {defineTypeOfTransport(item)}
           <Link href='https://www.booking.com/index.en-gb.html' target="_blank" rel="noreferrer">
-            <Button variant="outlined" style={resultItemStyle.buyTicket} type="submit">
+            <Button variant="outlined" style={style.buyTicket} type="submit">
               Booking
             </Button>
           </Link>
           <Link href='https://www.hostelworldgroup.com' target="_blank" rel="noreferrer">
-            <Button variant="outlined" style={resultItemStyle.buyTicket} type="submit">
+            <Button variant="outlined" style={style.buyTicket} type="submit">
               Hostel world
             </Button>
           </Link>
         </Box>
-        <Typography style={resultItemStyle.price}>9 € </Typography>
+        <Typography style={style.price}>9 € </Typography>
       </Box>
     </Box>
   );
--- src/components/SearchResult/SearchResultView.jsx
+++ src/components/SearchResult/SearchResultView.jsx
@@ -12,10 +12,12 @@ import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import SearchResultItem from './SearchResultItem';
 import { Box } from '@material-ui/core';
 import { resultStyle } from './style';
-import CarSharing from "../../assets/car-sharing.png"
+import CarSharing from "../../assets/car-sharing.png";
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 export default function SearchResultView({ city }) {
   const items = [1, 2, 3, 4, 5]; //------- temporary items of transport TODO: use from database
+  const style = useMediaQuery('(max-width:650px)') ? resultStyle.sm : resultStyle.lg;
   return (
     <div style={{ marginTop: "20px" }}>
       <Accordion>
@@ -24,25 +26,33 @@ export default function SearchResultView({ city }) {
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Box style={resultStyle.box}>
-            <Box style={resultStyle.inline}>
-              <AirplanemodeActiveIcon fontSize='large' />
-              <TrainIcon fontSize='large' />
-              <DirectionsBusIcon fontSize='large' />
-              <DirectionsBoatIcon fontSize='large' />
-              <img src={CarSharing} alt='car-shearing' style={resultStyle.car} />
+          <Box style={style.box}>
+            <Box style={style.inline}>
+              <AirplanemodeActiveIcon style={style.icon} />
+              <TrainIcon style={style.icon} />
+              <DirectionsBusIcon style={style.icon} />
+              <DirectionsBoatIcon style={style.icon} />
+              <img src={CarSharing} alt='car-shearing' style={style.car} />
             </Box>
             <Typography>
               Kiev
-              <ArrowForwardIcon fontSize='small' sx={{ verticalAlign: "text-bottom" }} />
+              <ArrowForwardIcon style={style.arrow} />
               Wroclaw
-              <ArrowForwardIcon fontSize='small' sx={{ verticalAlign: "text-bottom" }} />
+              <ArrowForwardIcon style={style.arrow} />
+              Paris
+              <ArrowForwardIcon style={style.arrow} />
+              London
+              <ArrowForwardIcon style={style.arrow} />
+              London
+              <ArrowForwardIcon style={style.arrow} />
+              <span>Ivano-Frankivsk</span>
+              <ArrowForwardIcon style={style.arrow} />
               {city.properties.display_name}
             </Typography>
-            <Box style={resultStyle.bottomContainer}>
-              <Typography style={resultStyle.time}>8 h 36 m</Typography>
-              <Box style={resultStyle.priceContainer}>
-                <Typography style={resultStyle.price}>€ 100</Typography>
+            <Box style={style.bottomContainer}>
+              <Typography style={style.time}>8 h 36 m</Typography>
+              <Box style={style.priceContainer}>
+                <Typography style={style.price}>€ 100</Typography>
               </Box>
             </Box>
           </Box>
@@ -54,31 +64,31 @@ export default function SearchResultView({ city }) {
         </AccordionDetails>
       </Accordion>
       {/* TODO delete second accordion and use one to map the component from database */}
-      <Accordion style={resultStyle.shadow}>
+      <Accordion>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Box style={resultStyle.box}>
-            <Box style={resultStyle.inline}>
-              <AirplanemodeActiveIcon fontSize='large' />
-              <TrainIcon fontSize='large' />
-              <DirectionsBusIcon fontSize='large' />
-              <DirectionsBoatIcon fontSize='large' />
-              <img src={CarSharing} alt='car-shearing' style={resultStyle.car} />
+          <Box style={style.box}>
+            <Box style={style.inline}>
+              <AirplanemodeActiveIcon style={style.icon} />
+              <TrainIcon style={style.icon} />
+              <DirectionsBusIcon style={style.icon} />
+              <DirectionsBoatIcon style={style.icon} />
+              <img src={CarSharing} alt='car-shearing' style={style.car} />
             </Box>
             <Typography>
               Kiev
-              <ArrowForwardIcon fontSize='small' sx={{ verticalAlign: "text-bottom" }} />
+              <ArrowForwardIcon style={style.arrow} />
               Wroclaw
-              <ArrowForwardIcon fontSize='small' sx={{ verticalAlign: "text-bottom" }} />
+              <ArrowForwardIcon style={style.arrow} />
               {city.properties.display_name}
             </Typography>
-            <Box style={resultStyle.bottomContainer}>
-              <Typography style={resultStyle.time}>8 h 36 m</Typography>
-              <Box style={resultStyle.priceContainer}>
-                <Typography style={resultStyle.price}>€ 100</Typography>
+            <Box style={style.bottomContainer}>
+              <Typography style={style.time}>8 h 36 m</Typography>
+              <Box style={style.priceContainer}>
+                <Typography style={style.price}>€ 100</Typography>
               </Box>
             </Box>
           </Box>
