da53f0e1e823ac0ba8a64c68e061fa2b242f5ffe
drewge23 <screwnut152@gmail.com>
2023-03-14 15:31:28 +0200

    Implemented the Autocomplete component

--- src/components/CheapTripSearch/CheapTripSearch.jsx
+++ src/components/CheapTripSearch/CheapTripSearch.jsx
@@ -1,32 +1,68 @@
-import React from 'react';
-import AutoComplete from "../AutoComplete/AutoComplete";
-import CloseIcon from "@mui/icons-material/Close";
-import DoubleArrowIcon from "@mui/icons-material/DoubleArrow";
-import classes from "../SearchCheapTrip/SearchComponent.module.css";
-import {Button} from "@material-ui/core";
-import i18n from "../../i18n";
-import RoutesList from "../AutoComplete/RoutesList";
-import SearchResultView from "../SearchResult/SearchResultView";
-import SearchFailResultView from "../SearchResult/SearchFailResultView";
+import React, {useState} from 'react';
 import {useSelector} from "react-redux";
+import {Autocomplete, TextField} from "@mui/material";
 
 function CheapTripSearch(props) {
     const locations = useSelector(state => state.data.locations)
+    const locationsKeysSorted = function () {
+        if (!locations) return
+        let temp = {...locations}
+        return (Object.keys(temp)).sort((a, b) => {
+            return temp[a].name > temp[b].name ? 1 : -1
+        })
+    }()
 
+    const [from, setFrom] = useState('')
+    const [to, setTo] = useState('')
 
+    const fromOptions = locationsKeysSorted
+        ? locationsKeysSorted.map(key =>
+            ({
+                label: locations[key].name + ', ' + locations[key].country_name,
+                key: key
+            }))
+        : []
+    const toOptions = locationsKeysSorted
+        ? [{label: 'Anywhere', key: 0}, ...locationsKeysSorted.map(key => ({
+            label: locations[key].name + ', ' + locations[key].country_name,
+            key: key
+        }))]
+        : []
+
+    const cleanForm = () => {
+        setFrom('')
+        setTo('')
+    }
 
     return (
         <div>
-            <select name="from">
-                {locations && Object.keys(locations).map(key => (
-                    <option key={key}>{locations[key].name + ', ' + locations[key].country_name}</option>
-                ))}
-            </select>
-            <select name="to">
-                {locations && Object.keys(locations).map(key => (
-                    <option key={key}>{locations[key].name + ', ' + locations[key].country_name}</option>
-                ))}
-            </select>
+            <form action="">
+                <Autocomplete
+                    value={from}
+                    onChange={(e, newValue) => {
+                        setFrom(newValue);
+                    }}
+                    disablePortal
+                    blurOnSelect
+                    openOnFocus
+                    options={fromOptions}
+                    sx={{width: 300}}
+                    renderInput={(params) => <TextField {...params} label="From"/>}
+                />
+                <Autocomplete
+                    value={to}
+                    onChange={(e, newValue) => {
+                        setTo(newValue);
+                    }}
+                    disablePortal
+                    blurOnSelect
+                    openOnFocus
+                    options={toOptions}
+                    sx={{width: 300}}
+                    renderInput={(params) => <TextField {...params} label="To"/>}
+                />
+            </form>
+            <button onClick={cleanForm}>Clean form</button>
         </div>
     )
 }
