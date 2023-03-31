7180134b2ad342db6b3cbbed898fd199630984c7
drewge23 <screwnut152@gmail.com>
2023-03-31 16:07:56 +0300

    Additional information added

--- src/components/CheapTripSearch/TravelInfo.jsx
+++ src/components/CheapTripSearch/TravelInfo.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import locations from '../../cheapTripData/locations.json'
 import transport from '../../cheapTripData/transport.json'
+import additional_information from '../../cheapTripData/inner_jsons/80001.json'
 
 function TravelInfo({travelInfo}) {
     return (
@@ -17,6 +18,26 @@ function TravelInfo({travelInfo}) {
                         <p>transport: {transport[travelInfo.transport].name}</p>}
                     <p>price: {travelInfo.price} euro</p>
                     <p>time: {travelInfo.duration} minutes</p>
+                    {additional_information &&
+                        <div>
+                            <h5>Additional information</h5>
+                            <p><b>From: </b></p>
+                            <p>Air code: {additional_information.from.air_code}</p>
+                            <p>Station: {additional_information.from.station}</p>
+                            <p>Latitude: {additional_information.from.coords.lat}</p>
+                            <p>Longtitude: {additional_information.from.coords.lon}</p>
+                            <p><b>To: </b></p>
+                            <p>Air code: {additional_information.to.air_code}</p>
+                            <p>Station: {additional_information.to.station}</p>
+                            <p>Latitude: {additional_information.to.coords.lat}</p>
+                            <p>Longtitude: {additional_information.to.coords.lon}</p>
+                            <p><b>Info: </b></p>
+                            <p>Duration: {Math.floor(additional_information.duration_min / 60)}
+                                h {additional_information.duration_min % 60}m
+                            </p>
+                            <p>Distance: {additional_information.distance_km}km</p>
+                            <p>Frequency: {additional_information.frequency.info}</p>
+                        </div>}
                 </>
                 : <div>Loading</div>}
         </div>
