0da0f3a9202f1fa4678731efa9d769b749c8ab67
Ishchenko-Vladislav <patriq.968@gmail.com>
2023-04-07 10:43:42 +0300

    update autocomplete

--- src/components/CheapTripSearch/CheapTripSearch.jsx
+++ src/components/CheapTripSearch/CheapTripSearch.jsx
@@ -11,6 +11,7 @@ import DoubleArrowIcon from "@mui/icons-material/DoubleArrow";
 import {Button} from "@material-ui/core";
 import i18n from "../../i18n";
 import { lowerCase } from 'lodash';
+import { asyncAutocomplete } from './asyncAutocomplete';
 
 function CheapTripSearch(props) {
     const routes = {...flying_routes, ...fixed_routes, ...common_routes}
@@ -78,79 +79,18 @@ function CheapTripSearch(props) {
     }
 
     const PAGINATION_LIMIT = 10
-    const PING_LIMIT = 1000
-    // function to check ping
-    const ping = async () => {
-		const start = Date.now()
-		await fetch('photon.komoot.io/api')
-        const end = Date.now()
-        return end - start
-
-	}
-    // function to get distance between points
-    function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-        var R = 6371; // Radius of the earth in km
-        var dLat = deg2rad(lat2-lat1);  // deg2rad below
-        var dLon = deg2rad(lon2-lon1);
-        var a =
-        Math.sin(dLat/2) * Math.sin(dLat/2) +
-        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-        Math.sin(dLon/2) * Math.sin(dLon/2);
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        var d = R * c; // Distance in km
-        return d;  // distance returned
-     }
-     function deg2rad(deg) {
-        return deg * (Math.PI/180)
-     }
-    //  get array ellemen by input value
-    const findAutocomplete = async (cityName) => {
-		///****active after change input********* */
-		const url = `https://photon.komoot.io/api/?q=${cityName}&osm_tag=place:city`;
-		const response = await fetch(url);
-		let data = (await response.json()).features;
-		return data;
-	};
-    const asycnAutocomplete = async(e, setState) => {
-        const p = await ping()
-        // check ping
-        if(p <= PING_LIMIT) {
-            const value = e.target.value
-            let data = await findAutocomplete(value)
-            // get geolocation 
-            navigator.geolocation.getCurrentPosition(function(position) {
-                setGeoLocation({
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude
-                })
-            });
-            // create array with needs fields
-            let array = data.map(item => ({
-                label: item.properties.name,
-                key: fromOptions.find(el => el.label.includes(item.properties.name))?.key || null,
-                latitude: item.geometry.coordinates[1],
-                longitude: item.geometry.coordinates[0],
-            }))
-            for(let i = 0; i< array.length; i++) {
-                let distance = getDistanceFromLatLonInKm(parseInt(geoLocation.latitude), 
-                parseInt(geoLocation.longitude),
-                array[i].latitude,
-                array[i].longitude); 
-                array[i].distance = distance;
-             }
-            //  sort array by distance
-             array = array.sort(function(a, b) {
-                return a.distance - b.distance
-             });
-            //  get array unique elements
-            const uniqueArray =  [...new Map(array.map(item =>
-                [item['label'], item])).values()]; 
-            setState(uniqueArray)
-        } else {
-            setState([])
-
-        }
+    const startAsyncAutocomplete = (e, setState, options) => {
+        // get geolocation
+        navigator.geolocation.getCurrentPosition(function(position) {
+            setGeoLocation({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            })
+        });
+        asyncAutocomplete(e, setState, options, geoLocation)
     }
+
+
     const checkFromOption = asyncFromOptions.length !== 0 ? asyncFromOptions : fromOptions
     const checkToOption = asyncToOptions.length !== 0 ? asyncToOptions : toOptions
     return (
@@ -162,7 +102,7 @@ function CheapTripSearch(props) {
                         setFrom(newValue ? newValue.label : '')
                         setFromKey(newValue.key ? newValue.key : '')
                     }}
-                    onInputChange={(e) => asycnAutocomplete(e, setAsyncFromOptions)}
+                    onInputChange={(e) => startAsyncAutocomplete(e, setAsyncFromOptions, fromOptions)}
                     disablePortal
                     blurOnSelect
                     openOnFocus
@@ -177,7 +117,7 @@ function CheapTripSearch(props) {
                         setTo(newValue ? newValue.label : '')
                         setToKey(newValue ? newValue.key : '')
                     }}
-                    onInputChange={(e) => asycnAutocomplete(e, setAsyncToOptions )}
+                    onInputChange={(e) => startAsyncAutocomplete(e, setAsyncToOptions, toOptions)}
                     disablePortal
                     blurOnSelect
                     openOnFocus
