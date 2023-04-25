e45b76d07315222420753820440fab6000118bd4
AntonLovkin <anton28111987@gmail.com>
2023-04-25 12:59:19 +0300

    viber btn

--- src/components/SocialButtons/SocialButtons.jsx
+++ src/components/SocialButtons/SocialButtons.jsx
@@ -12,17 +12,16 @@ export default function SocialButtons() {
             
             <a href='https://wa.me/972545779239' class="social-button whats-but" rel="noreferrer" target='_blank'><FaWhatsapp className='whatsapp' /></a>
 
-
             <a href='tel:+972545779239' class="social-button phone-but"><FaPhoneAlt className='phone' /></a>
-
 
             {/* <a href="https://www.messenger.com/t/rmant7" rel="noreferrer" target='_blank'><img src={FacebookMessenger} className='facebook' alt='facebook messenger icon' /></a> */}
 {/* 
             <a href="viber://chat?number=+972545779239" rel="noreferrer" target='_blank'><FaViber className='viber' /></a> */}
 
-            {/* <a href="viber://add?number=972545779239" class="social-button viber-but" ><FaViber className='viber-mb' /></a> */}
-
-            
+            {/* mobile version */}
+            <a href="viber://add?number=972545779239" class="social-button viber-but" ><FaViber className='viber-mb' /></a>
+            {/* desctop version */}
+            {/* <a href="viber://chat?number=+972545779239" class="social-button viber-but" ><FaViber className='viber-mb' /></a> */}           
         </div>
     )
 }
