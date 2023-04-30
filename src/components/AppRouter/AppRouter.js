94bf5a54b53af494227a93562d55d94861f1c1de
AlexAx11 <alexaxyanov@gmail.com>
2023-04-30 16:46:36 +0300

    new branch

--- src/components/AppRouter/AppRouter.js
+++ src/components/AppRouter/AppRouter.js
@@ -7,7 +7,7 @@ const AppRouter = () => {
   return (
     <Switch>
       {publicRoutes.map(({ path, Component }) => (
-        <Route key={path} path={path} component={Component} exact />
+        <Route key={path} path={path} component={Component} exact/>
       ))}
       <Redirect to={MAIN_ROUTE} />
     </Switch>
