104472504798bd6ebc325486f7f604df78559c87
AlisaRakhman <alisa.rakhman1@gmail.com>
2024-03-03 21:21:03 +0300

    Исправлено 'undefined'в названии стран

--- src/new_achitecture/modules/trip_search/domain/entites/CheapTripSearch/CheapTripSearch.jsx
+++ src/new_achitecture/modules/trip_search/domain/entites/CheapTripSearch/CheapTripSearch.jsx
@@ -38,7 +38,7 @@ function CheapTripSearch(props) {
     //console.log(routesForRender);
 
     const locationsKeysSorted = function () {
-        if (!locations) return
+        if (!locations) return;
         let temp = {...locations}
         return (Object.keys(temp)).sort((a, b) => {
             return temp[a].name > temp[b].name ? 1 : -1
@@ -56,14 +56,14 @@ function CheapTripSearch(props) {
     const fromOptions = locationsKeysSorted
         ? locationsKeysSorted.map(key =>
             ({
-                label: locations[key].name + ', ' + locations[key].country_name,
+                label: locations[key].name + [locations[key].country_name ? ', ' + locations[key].country_name : ''],
                 key: key
             }))
         : []
     const toOptions = locationsKeysSorted
         ? [{label: 'Anywhere', key: '0'}, ...locationsKeysSorted.map(key => ({
             label: key !== '0'
-                ? locations[key].name + ', ' + locations[key].country_name
+                ? locations[key].name + [locations[key].country_name ?  ', ' + locations[key].country_name : '']
                 : '',
             key: key
         }))]
