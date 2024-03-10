eec8d266423130b26c390c925b26e668a1d61306
Vladimir Baraniuk <norb4kivlad@gmail.com>
2024-03-10 12:23:23 +0200

    fix: remove unused loadingUploadTransfers from loadingSlice.js

--- src/new_achitecture/modules/trip_search/presentation/redux/slices/loadingSlice.js
+++ src/new_achitecture/modules/trip_search/presentation/redux/slices/loadingSlice.js
@@ -4,18 +4,16 @@ const loadingSlice = createSlice({
     name: 'loading',
     initialState: {
         isLoadingTransfers: false,
-        isLoadingNextTransfers: false,
-        isLoadingNewTransfer: false,
+        isLoadingNextTransfers: false
     },
     reducers: {
         setLoadingTransfers: (state, action) => ({...state, isLoadingTransfers: action.payload}),
-        setLoadingNextTransfers: (state, action) => ({...state, isLoadingNextTransfers: action.payload}),
-        setLoadingUploadTransfers: (state, action) => ({...state, isLoadingNewTransfer: action.payload})
+        setLoadingNextTransfers: (state, action) => ({...state, isLoadingNextTransfers: action.payload})
     }
 })
 
 const {actions, reducer} = loadingSlice;
 export default reducer;
 export const {
-    setLoadingTransfers, setLoadingNextTransfers, setLoadingUploadTransfers
+    setLoadingTransfers, setLoadingNextTransfers
 } = actions;
