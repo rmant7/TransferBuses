7bf712d8a809b182378053991cd5598e7da3caaf
Vladimir Baraniuk <norb4kivlad@gmail.com>
2024-03-10 11:38:18 +0200

    refactor: move app slice to new folder, fix transfer selector

--- src/new_achitecture/general/redux/selectors.js
+++ src/new_achitecture/general/redux/selectors.js
@@ -2,6 +2,6 @@ export const getCurrency = (state) => state.app.currency;
 export const getLanguage = (state) => state.app.lang;
 export const getSidebar = (state) => state.app.sidebar;
 //selector for non-existing field
-export const getTransfersData = (state) => state.transfersData;
+export const getTransfersData = (state) => state.transfers;
 export const getLoading = (state) => state.loading;
 export const getInputFromToCity = (state) => state.inputFromToCity;
