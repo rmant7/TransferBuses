f8f182e2da07d5fd6aee7046e6c5b6dda45add78
Vladimir Baraniuk <norb4kivlad@gmail.com>
2024-03-11 16:08:06 +0200

    Revert "fix: remove unused loadingUploadTransfers from loadingSlice.js"
    This reverts commit eec8d266423130b26c390c925b26e668a1d61306.

--- src/new_achitecture/modules/trip_search/presentation/redux/slices/loadingSlice.js
+++ src/new_achitecture/modules/trip_search/presentation/redux/slices/loadingSlice.js
@@ -4,16 +4,18 @@ const loadingSlice = createSlice({
     name: 'loading',
     initialState: {
         isLoadingTransfers: false,
-        isLoadingNextTransfers: false
+        isLoadingNextTransfers: false,
+        isLoadingNewTransfer: false,
     },
     reducers: {
         setLoadingTransfers: (state, action) => ({...state, isLoadingTransfers: action.payload}),
-        setLoadingNextTransfers: (state, action) => ({...state, isLoadingNextTransfers: action.payload})
+        setLoadingNextTransfers: (state, action) => ({...state, isLoadingNextTransfers: action.payload}),
+        setLoadingUploadTransfers: (state, action) => ({...state, isLoadingNewTransfer: action.payload})
     }
 })
 
 const {actions, reducer} = loadingSlice;
 export default reducer;
 export const {
-    setLoadingTransfers, setLoadingNextTransfers
+    setLoadingTransfers, setLoadingNextTransfers, setLoadingUploadTransfers
 } = actions;
