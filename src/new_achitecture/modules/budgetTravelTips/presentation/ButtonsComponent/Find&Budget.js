167317ff6f80985f36bf47e4e925fec0ecbc1cf2
AleksPanda <149190602+AleksPanda@users.noreply.github.com>
2024-03-12 18:33:14 +0200

    Merged components of budget tips section

--- src/new_achitecture/modules/budgetTravelTips/presentation/ButtonsComponent/Find&Budget.js
+++ src/new_achitecture/modules/budgetTravelTips/presentation/ButtonsComponent/Find&Budget.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from "./FindBudget.module.css";
 import {dataButtons} from "./dataButtons";
 
+
 const FindBudget = () => {
     const data = dataButtons[0];
     return (
--- src/new_achitecture/modules/budgetTravelTips/presentation/ButtonsComponent/dataButtons.js
+++ src/new_achitecture/modules/budgetTravelTips/presentation/ButtonsComponent/dataButtons.js
@@ -1,4 +1,4 @@
-import {TRAVEL_TIPS} from "../../../trip_search/domain/entites/utils/constants/constants";
+import {TRAVEL_TIPS} from "../../trip_search/domain/entites/utils/constants/constants";
 
 export const dataButtons = [{
     bookingLink: "https://www.booking.com/searchresults.en.html?aid=7920152&city=-370374",
